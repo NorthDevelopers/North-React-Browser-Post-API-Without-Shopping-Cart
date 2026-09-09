ea99c0d4244ef304cacc5e723e9ae4db8fd9cf4d
wesleycenatus <wcenatus@nabancard.com>
2026-09-09 14:15:12 -0400

    Change Browser Post title to Embedded checkout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NavBar() {
   return (
     <><h1 className='text-center mt-4'>Payments Hub Developers</h1>
-    <h3 className='text-center mt-4'>Browser Post API Sample Store</h3></>
+    <h3 className='text-center mt-4'>Embedded Checkout Sample Store</h3></>
   )
 }
 
